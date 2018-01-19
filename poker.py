52369ddfe2a8105af24af463cbcc9e73550e0a47
Brian Caffey <briancaffey2010@gmail.com>
2018-01-19 13:06:04 -0500

    added poker script

---
+++ poker.py
@@ -0,0 +1,127 @@
+
+
+from itertools import combinations
+from collections import defaultdict
+import sys
+
+card_order_dict = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "T":10,"J":11, "Q":12, "K":13, "A":14}
+
+def check_straight_flush(hand):
+    if check_flush(hand) and check_straight(hand):
+        return True
+    else:
+        return False
+
+def check_four_of_a_kind(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda:0)
+    for v in values: 
+        value_counts[v]+=1
+    if sorted(value_counts.values()) == [1,4]:
+        return True
+    return False
+
+def check_full_house(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda:0)
+    for v in values: 
+        value_counts[v]+=1
+    if sorted(value_counts.values()) == [2,3]:
+        return True
+    return False
+
+def check_flush(hand):
+    suits = [i[1] for i in hand]
+    if len(set(suits))==1:
+        return True
+    else:
+        return False
+
+def check_straight(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda:0)
+    for v in values:
+        value_counts[v] += 1
+    rank_values = [card_order_dict[i] for i in values]
+    value_range = max(rank_values) - min(rank_values)
+    if len(set(value_counts.values())) == 1 and (value_range==4):
+        return True
+    else: 
+        #check straight with low Ace
+        if set(values) == set(["A", "2", "3", "4", "5"]):
+            return True
+        return False
+
+def check_three_of_a_kind(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda:0)
+    for v in values:
+        value_counts[v]+=1
+    if set(value_counts.values()) == set([3,1]):
+        return True
+    else:
+        return False
+
+def check_two_pairs(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda:0)
+    for v in values:
+        value_counts[v]+=1
+    if sorted(value_counts.values())==[1,2,2]:
+        return True
+    else:
+        return False
+
+def check_one_pairs(hand):
+    values = [i[0] for i in hand]
+    value_counts = defaultdict(lambda:0)
+    for v in values:
+        value_counts[v]+=1
+    if 2 in value_counts.values():
+        return True
+    else:
+        return False
+
+    
+def check_hand(hand):
+    if check_straight_flush(hand):
+        return 9
+    if check_four_of_a_kind(hand):
+        return 8
+    if check_full_house(hand):
+        return 7
+    if check_flush(hand):
+        return 6
+    if check_straight(hand):
+        return 5
+    if check_three_of_a_kind(hand):
+        return 4
+    if check_two_pairs(hand):
+        return 3
+    if check_one_pairs(hand):
+        return 2
+    #always return highest card if no higher hands are found
+    return 1
+
+
+hand_dict = {9:"straight-flush", 8:"four-of-a-kind", 7:"full-house", 6:"flush", 5:"straight", 4:"three-of-a-kind", 3:"two-pairs", 2:"one-pair", 1:"highest-card"}
+#exhaustive search using itertools.combinations
+def play(cards):
+    hand = cards[:5]
+    deck = cards[5:]
+    best_hand = 0
+    for i in range(6):
+        possible_combos = combinations(hand, 5-i)
+        for c in possible_combos: 
+            current_hand = list(c) + deck[:i]
+            hand_value = check_hand(current_hand)
+            if hand_value > best_hand:
+                best_hand = hand_value
+                
+    return hand_dict[best_hand]
+
+for i in sys.stdin.readlines():
+    cards = list(map(lambda x:x, i.split()))
+    hand = cards[:5]
+    deck = cards[5:]
+    print("Hand:", " ".join(hand), "Deck:", " ".join(deck), "Best hand:", play(cards))
